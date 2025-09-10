23c4a3e0d43a7e40f048e7f6a107bc350b8e73ef
NikRusDev <grumiktiktok@gmail.com>
2025-09-10 12:38:03 +0800

    Delete solution/task1.sh

--- solution/task1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir data/arch
-mkdir data/arch/archtar
-mkdir data/arch/archzip
-tar -xf data/archive-part1.tar -C data/arch/archtar
-unzip data/archive-part2.zip -d data/arch/archzip
-tar -czvf data/archive-combined.tar.gz data/arch
